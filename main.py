661c9b3bda385830323cb9274dd8a35ab1930cba
B.Yahia <k4edjyah@gmail.com>
2023-09-13 18:37:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 @app.get("/items/")
 async def read_items(q: Annotated[Union[list[str], None], Query()] = None):
     query_items = parse_input(q) 
+    
     return query_items
 
 
@@ -15,7 +16,15 @@ def parse_input(input_data):
     try:
         # Convert the input JSON-like string into a Python dictionary
         data = json.loads(input_data[0])
-        
+        # Convert the values in the first two keys to integers
+        for key in data.keys():
+            if key in ["numCertificates", "disContinuousPeriods"]:
+                try:
+                    data[key] = int(float(data[key]))
+                except ValueError:
+                    # Handle the case where the conversion is not possible
+                    pass
+                
         # Extract and return the dictionary
         return data
     except Exception as e:
