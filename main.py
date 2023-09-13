b359db81723213c38aec09a2922d6e98e0d9eeee
B.Yahia <k4edjyah@gmail.com>
2023-09-13 17:15:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-
+"""write a FastAPI code that accepts 3 int parameters through fastapi and return the sum of the 3 int parameters """
 app = FastAPI()
+def sum(a,b,c):
+    return a+b+c
+@app.get("/sum/{a}/{b}/{c}")
+async def root(a: int, b: int, c: int):
+    return {"sum": sum(a,b,c)}  
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
