efadba481a6c5ddb3fb906859f44818421b0d03f
B.Yahia <k4edjyah@gmail.com>
2023-09-13 18:33:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from typing import Annotated, Union
 
 from fastapi import FastAPI, Query
-
+import json
 app = FastAPI()
 
 
 @app.get("/items/")
 async def read_items(q: Annotated[Union[list[str], None], Query()] = None):
-    query_items = {"q": q}
+    query_items = parse_input(q) 
     return query_items
+
+
+def parse_input(input_data):
+    try:
+        # Convert the input JSON-like string into a Python dictionary
+        data = json.loads(input_data[0])
+        
+        # Extract and return the dictionary
+        return data
+    except Exception as e:
+        # Handle any exceptions that may occur during parsing
+        return {"error": str(e)}
+
