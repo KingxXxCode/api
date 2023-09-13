b42706960259c5ddfdf51b14c93869162c6726ea
B.Yahia <k4edjyah@gmail.com>
2023-09-13 17:40:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
-"""write a FastAPI code that accepts 3 int parameters through fastapi and return the sum of the 3 int parameters """
-app = FastAPI()
-def sum(a,b,c):
-    return a+b+c
-@app.get("/sum/{a}/{b}/{c}")
-async def root(a: int, b: int, c: int):
-    return {"sum": sum(a,b,c)}  
+from typing import Annotated, Union
 
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[Union[list[str], None], Query()] = None):
+    query_items = {"q": q}
+    return query_items
